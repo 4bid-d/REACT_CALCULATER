562f079a8f88e7d38c1a36fb0c76d09376695eb8
4bid-d <abidpponline@gmail.com>
2022-09-04 10:56:30 +0530

    style of calculater added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-import React, { useState ,createContext}  from 'react'
-import CurrencyRow from './CurrencyRow';
-import "./app.css"
-
+import React, {}  from 'react'
+import "./style.css"
 // import Pagination from './Pagination';
 function App() {
  return (  
     <>
-    <h1 >Currency converter</h1>
-    <CurrencyRow/>
-    <div>=</div>
-    <CurrencyRow/>
+  <div className="calculater-grid">
+    <div className="output">
+      <div className="previous-operand"></div>
+      <div className="current-operand"></div>
+    </div>
+    <button className="span-two">AC</button>
+    <button>DEL</button>
+    <button>/</button>
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+    <button>*</button>
+    <button>4</button>
+    <button>5</button>
+    <button>6</button>
+    <button>+</button>
+    <button>7</button>
+    <button>8</button>
+    <button>9</button>
+    <button>-</button>
+    <button>.</button>
+    <button>0</button>
+    <button className='span-two'>=</button>
+  </div>
     </>
   )
 
